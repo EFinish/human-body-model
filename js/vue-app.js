9281b7ba0550be43ba722b648a71091824440160
EFinish <EFinish@gmail.com>
2018-07-18 19:12:40 -0500

    more flushed out props

--- js/vue-app.js
+++ js/vue-app.js
@@ -4,64 +4,205 @@ var app = new Vue({
         measurements: {
             length: {
                 right_arm: {
-                    shoulder_to_bicep_tri: 0,
-                    bicep_tri_to_elbow: 0,
-                    elbow_to_forearm: 0,
-                    forearm_to_wrist: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        shoulder_to_bicep_tri: {
+                            value: 0,
+                            name: ""
+                        },
+                        bicep_tri_to_elbow: {
+                            value: 0,
+                            name: ""
+                        },
+                        elbow_to_forearm: {
+                            value: 0,
+                            name: ""
+                        },
+                        forearm_to_wrist: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 left_arm: {
-                    shoulder_to_bicep_tri: 0,
-                    bicep_tri_to_elbow: 0,
-                    elbow_to_forearm: 0,
-                    forearm_to_wrist: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        shoulder_to_bicep_tri: {
+                            value: 0,
+                            name: ""
+                        },
+                        bicep_tri_to_elbow: {
+                            value: 0,
+                            name: ""
+                        },
+                        elbow_to_forearm: {
+                            value: 0,
+                            name: ""
+                        },
+                        forearm_to_wrist: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 mid_body:{
-                    hip_to_waist: 0,
-                    waist_to_chest: 0,
-                    chest_to_collar: 0,
-                    collar_to_shoulder: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        hip_to_waist: {
+                            value: 0,
+                            name: ""
+                        },
+                        waist_to_chest: {
+                            value: 0,
+                            name: ""
+                        },
+                        chest_to_collar: {
+                            value: 0,
+                            name: ""
+                        },
+                        collar_to_shoulder: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 right_leg: {
-                    hip_to_inner_thigh: 0,
-                    inner_thigh_to_knee: 0,
-                    knee_to_calf: 0,
-                    calf_to_achilles: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        hip_to_inner_thigh: {
+                            value: 0,
+                            name: ""
+                        },
+                        inner_thigh_to_knee: {
+                            value: 0,
+                            name: ""
+                        },
+                        knee_to_calf: {
+                            value: 0,
+                            name: ""
+                        },
+                        calf_to_achilles: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 left_leg: {
-                    hip_to_inner_thigh: 0,
-                    inner_thigh_to_knee: 0,
-                    knee_to_calf: 0,
-                    calf_to_achilles: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        hip_to_inner_thigh: {
+                            value: 0,
+                            name: ""
+                        },
+                        inner_thigh_to_knee: {
+                            value: 0,
+                            name: ""
+                        },
+                        knee_to_calf: {
+                            value: 0,
+                            name: ""
+                        },
+                        calf_to_achilles: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 }
                 
             },
             circumference: {
                 right_arm: {
-                    bicep_tri: 0,
-                    elbow: 0,
-                    forearm: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        bicep_tri: {
+                            value: 0,
+                            name: ""
+                        },
+                        elbow: {
+                            value: 0,
+                            name: ""
+                        },
+                        forearm: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 left_arm: {
-                    bicep_tri: 0,
-                    elbow: 0,
-                    forearm: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        bicep_tri: {
+                            value: 0,
+                            name: ""
+                        },
+                        elbow: {
+                            value: 0,
+                            name: ""
+                        },
+                        forearm: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 mid_body:{
-                    hip: 0,
-                    waist: 0,
-                    chest: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        hip: {
+                            value: 0,
+                            name: ""
+                        },
+                        waist: {
+                            value: 0,
+                            name: ""
+                        },
+                        chest: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 right_leg: {
-                    inner_thigh: 0,
-                    knee: 0,
-                    calf: 0,
-                    achilles: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        inner_thigh: {
+                            value: 0,
+                            name: ""
+                        },
+                        knee: {
+                            value: 0,
+                            name: ""
+                        },
+                        calf: {
+                            value: 0,
+                            name: ""
+                        },
+                        achilles: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 },
                 left_leg: {
-                    inner_thigh: 0,
-                    knee: 0,
-                    calf: 0,
-                    achilles: 0
+                    name: "Right Arm",
+                    body_parts: {
+                        inner_thigh: {
+                            value: 0,
+                            name: ""
+                        },
+                        knee: {
+                            value: 0,
+                            name: ""
+                        },
+                        calf: {
+                            value: 0,
+                            name: ""
+                        },
+                        achilles: {
+                            value: 0,
+                            name: ""
+                        }
+                    }
                 }
             }
         },
