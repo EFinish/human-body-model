b804c9794a1e972ebe71c3b5158d2c7b972bbe93
EFinish <EFinish@gmail.com>
2018-07-24 18:18:26 -0500

    made inputs a little less nasty looking

--- js/input-components.js
+++ js/input-components.js
@@ -28,7 +28,7 @@ Vue.component(
             'body_part'
         ],
         template:
-        '<div class="col-sm-6">'+
+        '<div class="col-sm-6 form-group">'+
             '<label for="">Circumference for {{body_part.name}} </label>'+
             '<div class="input-group">' +
                 '<input type="number" v-model="body_part.value">'+    
